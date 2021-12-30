5b507bcac591b57d6a980ab666075cf9ac1924de
Kvnsky <violentfx1@gmail.com>
2021-12-30 20:42:38 +0100

    Update gamma command

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const OBSWebSocket = require('obs-websocket-js');
 const obs = new OBSWebSocket();
 require('dotenv').config();
 
-const CHANNEL_NAME = ['channelname'];
-const SOURCE_NAME = 'sourcename';
-const FILTER_NAME = 'filtername';
-
 obs
   .connect({
     address: process.env.WS_ADDRESS,
@@ -25,54 +21,45 @@ const client = new tmi.Client({
     username: process.env.BOT_USERNAME,
     password: process.env.BOT_OAUTH,
   },
-  channels: CHANNEL_NAME,
+  channels: [process.env.CHANNEL_NAME],
 });
 
 client.connect();
 
-client.on('message', (channel, user, message, self) => {
-  const isMod = user.mod || user['user-type'] === 'mod';
-  const isBroadcaster = channel.slice(1) === user.username;
-  const isModUp = isMod || isBroadcaster;
+client.on('message', (channel, tags, message, self) => {
+  const isModUp = tags.mod === true || channel.slice(1) === tags.username;
 
-  // Ignore echoed messages.
   if (self) return;
 
-  if (isModUp) {
-    if (message.toLowerCase() === '!gammaon') {
-      obs
-        .send('SetSourceFilterVisibility', {
-          sourceName: SOURCE_NAME,
-          filterName: FILTER_NAME,
-          filterEnabled: true,
-        })
-        .then(() => {
-          return obs.send('GetSourceFilterInfo', {
-            sourceName: SOURCE_NAME,
-            filterName: FILTER_NAME,
+  if (isModUp && message.toLowerCase() === '!gamma') {
+    obs
+      .send('GetSourceFilterInfo', {
+        sourceName: process.env.SOURCE_NAME,
+        filterName: process.env.FILTER_NAME,
+      })
+      .then((data) => {
+        if (data.enabled === true) {
+          obs.send('SetSourceFilterVisibility', {
+            sourceName: process.env.SOURCE_NAME,
+            filterName: process.env.FILTER_NAME,
+            filterEnabled: false,
           });
-        })
-        .then((data) => {
-          client.say(channel, `Gamma state set to: ${data.enabled}`);
-        });
-    }
-
-    if (message.toLowerCase() === '!gammaoff') {
-      obs
-        .send('SetSourceFilterVisibility', {
-          sourceName: SOURCE_NAME,
-          filterName: FILTER_NAME,
-          filterEnabled: false,
-        })
-        .then(() => {
-          return obs.send('GetSourceFilterInfo', {
-            sourceName: SOURCE_NAME,
-            filterName: FILTER_NAME,
+          client.say(channel, 'Gamma is now OFF');
+        } else {
+          obs.send('SetSourceFilterVisibility', {
+            sourceName: process.env.SOURCE_NAME,
+            filterName: process.env.FILTER_NAME,
+            filterEnabled: true,
           });
-        })
-        .then((data) => {
-          client.say(channel, `Gamma state set to: ${data.enabled}`);
-        });
-    }
+          client.say(channel, `Gamma is now ON at ${data.settings.gamma}`);
+        }
+      })
+      .catch((err) => {
+        client.say(channel, `Error: ${err.error}`);
+      });
   }
+});
+
+obs.on('error', (err) => {
+  console.log('socket error:', err);
 });
