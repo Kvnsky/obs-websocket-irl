7a4ee9a550a5db64c6e9373e166ceb7ff9cc7973
Kvnsky <violentfx1@gmail.com>
2022-01-04 05:02:21 +0100

    Add argument handler for commands and ability to change gamma value with command

--- index.js
+++ index.js
@@ -35,71 +35,95 @@ client.connect();
 
 client.on('message', (channel, tags, message, self) => {
   const isModUp = tags.mod === true || channel.slice(1) === tags.username;
+  const [command, ...args] = message.toLowerCase().split(' ');
 
   if (self) return;
 
-  if (isModUp && message.toLowerCase() === '!gamma') {
-    obs
-      .send('GetSourceFilterInfo', {
-        sourceName: process.env.SOURCE_NAME,
-        filterName: process.env.FILTER_NAME,
-      })
-      .then((data) => {
-        if (data.enabled === true) {
-          obs.send('SetSourceFilterVisibility', {
+  if (isModUp && command === '!gamma') {
+    if (args[0] === undefined) {
+      obs
+        .send('GetSourceFilterInfo', {
+          sourceName: process.env.SOURCE_NAME,
+          filterName: process.env.FILTER_NAME,
+        })
+        .then((data) => {
+          if (data.enabled === true) {
+            obs.send('SetSourceFilterVisibility', {
+              sourceName: process.env.SOURCE_NAME,
+              filterName: process.env.FILTER_NAME,
+              filterEnabled: false,
+            });
+            client.say(channel, 'Gamma is now OFF');
+          } else {
+            obs.send('SetSourceFilterVisibility', {
+              sourceName: process.env.SOURCE_NAME,
+              filterName: process.env.FILTER_NAME,
+              filterEnabled: true,
+            });
+            client.say(channel, `Gamma is now ON at ${data.settings.gamma}`);
+          }
+        })
+        .catch((err) => {
+          client.say(channel, `Error: ${err.error}`);
+        });
+    } else if (!isNaN(args[0])) {
+      obs
+        .send('SetSourceFilterSettings', {
+          sourceName: process.env.SOURCE_NAME,
+          filterName: process.env.FILTER_NAME,
+          filterSettings: { gamma: Number(args[0]) },
+        })
+        .then(() => {
+          return obs.send('GetSourceFilterInfo', {
             sourceName: process.env.SOURCE_NAME,
             filterName: process.env.FILTER_NAME,
-            filterEnabled: false,
           });
-          client.say(channel, 'Gamma is now OFF');
-        } else {
-          obs.send('SetSourceFilterVisibility', {
-            sourceName: process.env.SOURCE_NAME,
-            filterName: process.env.FILTER_NAME,
-            filterEnabled: true,
-          });
-          client.say(channel, `Gamma is now ON at ${data.settings.gamma}`);
-        }
-      })
-      .catch((err) => {
-        client.say(channel, `Error: ${err.error}`);
-      });
+        })
+        .then((data) => {
+          client.say(channel, `Gamma changed to ${data.settings.gamma}`);
+        })
+        .catch((err) => {
+          client.say(channel, `Error: ${err}`);
+        });
+    }
   }
 
-  if (isModUp && message.toLowerCase() === '!flip') {
-    obs
-      .send('GetSceneItemProperties', { item: process.env.SOURCE_NAME })
-      .then((data) => {
-        if (
-          data.height === -1080 &&
-          data.width === -1920 &&
-          data.position.x === 1920 &&
-          data.position.y === 1080 &&
-          data.scale.x === -1 &&
-          data.scale.y === -1
-        ) {
-          obs.send('SetSceneItemProperties', {
-            item: process.env.SOURCE_NAME,
-            height: 1080,
-            width: 1920,
-            position: { x: 0, y: 0 },
-            scale: { x: 1, y: 1 },
-          });
-          client.say(channel, 'Source is now NORMAL');
-        } else {
-          obs.send('SetSceneItemProperties', {
-            item: process.env.SOURCE_NAME,
-            height: -1080,
-            width: -1920,
-            position: { x: 1920, y: 1080 },
-            scale: { x: -1, y: -1 },
-          });
-          client.say(channel, 'Source is now FLIPPED');
-        }
-      })
-      .catch((err) => {
-        client.say(channel, `Error: ${err.error}`);
-      });
+  if (isModUp && command === '!flip') {
+    if (args[0] === undefined) {
+      obs
+        .send('GetSceneItemProperties', { item: process.env.SOURCE_NAME })
+        .then((data) => {
+          if (
+            data.height === -1080 &&
+            data.width === -1920 &&
+            data.position.x === 1920 &&
+            data.position.y === 1080 &&
+            data.scale.x === -1 &&
+            data.scale.y === -1
+          ) {
+            obs.send('SetSceneItemProperties', {
+              item: process.env.SOURCE_NAME,
+              height: 1080,
+              width: 1920,
+              position: { x: 0, y: 0 },
+              scale: { x: 1, y: 1 },
+            });
+            client.say(channel, 'Source is now NORMAL');
+          } else {
+            obs.send('SetSceneItemProperties', {
+              item: process.env.SOURCE_NAME,
+              height: -1080,
+              width: -1920,
+              position: { x: 1920, y: 1080 },
+              scale: { x: -1, y: -1 },
+            });
+            client.say(channel, 'Source is now FLIPPED');
+          }
+        })
+        .catch((err) => {
+          client.say(channel, `Error: ${err.error}`);
+        });
+    }
   }
 });
 
